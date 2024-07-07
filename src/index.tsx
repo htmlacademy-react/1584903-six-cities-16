a685fa10e450d3204c83bf3fe96c5a1967ff7889
g1uk <tartatushka1@gmail.com>
2024-07-07 18:42:45 +0300

    add all pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app/app.tsx';
+import Settings from './app/settings.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <h1>Hello, World!</h1>
-  </React.StrictMode>
+  <App
+    offersCount={Settings.OffersCount}
+  />
 );
