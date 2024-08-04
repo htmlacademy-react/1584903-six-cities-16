9110a1e73f6239f16cea4a043f1e9409e3fc4cc1
g1uk <tartatushka1@gmail.com>
2024-08-04 15:54:20 +0300

    add sort

--- src/features/rental/rentalSlice.ts
+++ src/features/rental/rentalSlice.ts
@@ -1,9 +1,9 @@
 import {OfferCardType} from '../../types/offer.ts';
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {AppDispatch} from '../../types/state.ts';
+import {createSelector, createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {AppDispatch, State} from '../../types/state.ts';
 import {offers} from '../../mocks/offers.tsx';
 
-export type SortOption = 'Popular' | 'PriceLowToHigh' | 'PriceHighToLow';
+export type SortOption = 'Popular' | 'Price: low to high' | 'Price: high to low' | 'Top rated first';
 
 type RentalState = {
   currentCity: string;
@@ -38,5 +38,22 @@ export const {setCurrentCity, setOffers, setSortOption} = rentalSlice.actions;
 export const getOffers = () => (dispatch: AppDispatch) => {
   dispatch(setOffers(offers));
 };
+
+export const selectedSortedOffers = createSelector(
+  (state: State) => state.offers,
+  (state: State) => state.sortOption,
+  (offerCards, sortOption) => {
+    switch (sortOption) {
+      case 'Price: low to high':
+        return [...offerCards].sort((a, b) => a.price - b.price);
+      case 'Price: high to low':
+        return [...offerCards].sort((a, b) => b.price - a.price);
+      case 'Top rated first':
+        return [...offerCards].sort((a, b) => b.rating - a.rating);
+      default:
+        return offerCards;
+    }
+  }
+);
 
 export const {reducer} = rentalSlice;
